0f4bdb87895d5215095fb66325bd42c6b30b6d34
mei <yimei.wym@gmail.com>
2019-10-03 11:43:25 +0800

    check only the row and col of the grid that was clicked on

--- gameHelpers.mjs
+++ gameHelpers.mjs
@@ -55,6 +55,8 @@ export const insertMarker = (e, currentPlayer, boardState) => {
 
   // Keep track of the current board state
   boardState[parseInt(row)][parseInt(col)] = currentPlayer;
+
+  console.log(boardState);
 };
 
 export const checkWinIter = (boardState, currentPlayer) => {
@@ -125,9 +127,41 @@ export const checkWinIter = (boardState, currentPlayer) => {
 
 export const checkWin = (e, boardState) => {
   // Get the data attributes
-  let row = e.target.dataset.row;
-  let col = e.target.dataset.col;
+  let row = parseInt(e.target.dataset.row);
+  let col = parseInt(e.target.dataset.col);
+
   let currentPlayer = e.target.innerHTML;
   console.log(row);
-  // let col = e.target.dataset.col;
+  console.log(col);
+
+  // To check the row, you would want to move from left to right, i.e. iterate through the col values
+  // Only check the row (left to right) where the click occurred
+  for (let c = 0; c < dimensions; i++) {
+    // Check if each grid contains the currentPlayer's marker
+    // If it doesn't, move on to the next column
+    if (boardState[row][i] !== currentPlayer) {
+      break;
+    }
+
+    // If the loop hasn't broken and c === 2 (i.e., the end of the row, it means that every grid in this current row contained the currentPlayer's marker)
+    if (c === dimensions - 1) {
+      return true;
+    }
+  }
+
+  // Only check the col where the click occurred
+  for (let r = 0; r < dimensions; r++) {
+    if (boardState[r][col] !== currentPlayer) {
+      break;
+    }
+
+    if (r === dimensions - 1) {
+      return true;
+    }
+  }
+
+  // Only check diagonal if row===col
+  if (row === col)
+    // Only check anti-diagonal if row+col === 2
+    return false;
 };
